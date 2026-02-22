9938256c548dd8938228d4a809591f2db17e08fc
Anuj Kumar <anujpandey61687@gmail.com>
2026-02-22 23:35:01 +0530

    Add gaussian blur effect

---
+++ gaussian_blur.py
@@ -0,0 +1,8 @@
+import cv2
+img = cv2.imread("images/Recent.png")
+blurred = cv2.GaussianBlur(img, (7, 7), 0)
+
+cv2.imshow("Original Image : ", img)
+cv2.imshow("Gaussian Blur : ", blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
