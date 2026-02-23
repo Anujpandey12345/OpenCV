8047d0f3378cef5f8b024841e60795e65d1836b3
Anuj Kumar <anujpandey61687@gmail.com>
2026-02-23 23:19:36 +0530

    Chaneing

--- gaussian_blur.py
+++ gaussian_blur.py
@@ -1,6 +1,6 @@
 import cv2
 img = cv2.imread("images/Recent.png")
-blurred = cv2.GaussianBlur(img, (7, 7), 0)
+blurred = cv2.GaussianBlur(img, (7, 7), 888)
 
 cv2.imshow("Original Image : ", img)
 cv2.imshow("Gaussian Blur : ", blurred)
