2271378bda31fdf2f44d19c705c0821f63b59578
Anuj Kumar <anujpandey61687@gmail.com>
2026-02-26 22:42:06 +0530

    Chnage some

--- gaussian_blur.py
+++ gaussian_blur.py
@@ -1,8 +1,8 @@
-import cv2
-img = cv2.imread("images/Recent.png")
-blurred = cv2.GaussianBlur(img, (7, 7), 888)
+# import cv2
+# img = cv2.imread("images/Recent.png")
+# blurred = cv2.GaussianBlur(img, (7, 7), 888)
 
-cv2.imshow("Original Image : ", img)
-cv2.imshow("Gaussian Blur : ", blurred)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("Original Image : ", img)
+# cv2.imshow("Gaussian Blur : ", blurred)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
